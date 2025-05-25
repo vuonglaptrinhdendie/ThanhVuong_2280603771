f6429fc5a619049299d4d47c2206b1999637a558
vuonglaptrinhdendie <nguyenthanhvuong1303@gmail.com>
2025-05-25 21:07:54 +0700

    add lab02  railfence

--- Lap02/api.py
+++ Lap02/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 
 app = Flask(__name__)
 
@@ -10,12 +11,15 @@ caesar_cipher = CaesarCipher()
 # VIGENERE CIPHER ALGORITHM
 vigenere_cipher = VigenereCipher()
 
+# RAIL FENCE CIPHER ALGORITHM
+railfence_cipher = RailFenceCipher()
+
 # Endpoint cho Caesar Cipher - Mã hóa
 @app.route('/api/caesar/encrypt', methods=['POST'])
 def caesar_encrypt():
     data = request.json
     plain_text = data['plain_text']
-    key = int(data['key'])  # Caesar dùng key là số
+    key = int(data['key'])
     encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
     return jsonify({'encrypted_message': encrypted_text})
 
@@ -33,7 +37,7 @@ def caesar_decrypt():
 def vigenere_encrypt():
     data = request.json
     plain_text = data['plain_text']
-    key = data['key']  # Vigenère dùng key là chuỗi
+    key = data['key']
     encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
     return jsonify({'encrypted_message': encrypted_text})
 
@@ -44,6 +48,24 @@ def vigenere_decrypt():
     cipher_text = data['cipher_text']
     key = data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+# Endpoint cho Rail Fence Cipher - Mã hóa
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def railfence_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+# Endpoint cho Rail Fence Cipher - Giải mã
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def railfence_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
 if __name__ == "__main__":
